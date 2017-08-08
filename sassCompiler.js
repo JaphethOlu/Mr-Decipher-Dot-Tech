928e8edc6be6811570343212cb4a653287082667
Ishola Japheth <japhethish@gmail.com>
2017-08-08 12:00:52 +0100

    Added Chalk

--- sassCompiler.js
+++ sassCompiler.js
@@ -1,16 +1,17 @@
 const sass = require("node-sass");
 const fs = require("fs");
+const chalk = require("chalk");
 
 sass.render({
     file: "./src/srcBuild/styles/styles.scss",
     outFile: "./src/production/css"
 }, (err, result) => {
     if(!err) {
-        console.log("No errors during sass styles compilation");
+        console.log(chalk.magenta("No errors during sass styles compilation"));
         fs.writeFile("./src/production/css/styles.css", result.css, (err) => {
             if(!err) {
                 /*eslint-disable no-console*/
-                console.log("Successfully compiles sass Styles");
+                console.log(chalk.green.bold("Successfully produced css file ;)"));
             }
         });
     }
